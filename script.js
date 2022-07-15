92d7f4d136bb79d3ecc6ec2834a0fcdbaa3e861b
João Tápparo <joaovtapparo@gmail.com>
2022-07-15 00:35:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function load(){
     var date = new Date()
     var hora = date.getHours()
     msg.innerHTML = `now it's ${hora} o'clock`
-    if (hora >= 0 && hora <= 12) {
+    if (hora >= 5 && hora <= 12) {
         img.src = 'images/day.png'
     }
     else if (hora >= 12 && hora <= 18) {
